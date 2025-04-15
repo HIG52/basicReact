8ca87c9a99e51f1014a650f32d94423ebd3e062f
HIG <hingjy1997@gmail.com>
2025-04-15 16:14:28 +0900

    feat: update routing configuration in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
-import Home from './pages/Home';
 import Board from './pages/Board';
 import QnA from './pages/QnA';
 import Notice from './pages/Notice';
+import Main from './pages/Main';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route index element={<Main />} />
           <Route path="board" element={<Board />} />
           <Route path="qna" element={<QnA />} />
           <Route path="notice" element={<Notice />} />

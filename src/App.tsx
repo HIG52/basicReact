fbd28e4ba738be205ceedd0e4217a887aa183589
HIG <hingjy1997@gmail.com>
2025-04-22 10:33:38 +0900

    feat: update App component with new routes and components

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Layout from './components/layout/Layout';
 import Board from './pages/board/Board';
 import QnA from './pages/qna/QnA';
 import Notice from './pages/notice/Notice';
+import NoticeDetail from './pages/notice/NoticeDetail';
 import Main from './pages/main/Main';
 
 const App: React.FC = () => {
@@ -14,7 +15,8 @@ const App: React.FC = () => {
           <Route index element={<Main />} />
           <Route path="board" element={<Board />} />
           <Route path="qna" element={<QnA />} />
-          <Route path="notice" element={<Notice />} />
+          <Route path="notices" element={<Notice />} />
+          <Route path="notices/:id" element={<NoticeDetail />} />
         </Route>
       </Routes>
     </Router>

1d1a8ec51340421344d328005f6dcceca81fe9af
HIG <hingjy1997@gmail.com>
2025-06-12 17:25:34 +0900

    feat: 관리자 페이지 통합 index.ts 파일 추가

---
+++ src/pages/admin/index.ts
@@ -0,0 +1,14 @@
+// User domain
+export * from './user';
+
+// Notice domain  
+export * from './notice';
+
+// QnA domain
+export * from './qna';
+
+// Board domain
+export * from './board';
+
+// Admin main
+export { default as AdminMain } from './AdminMain'; 
